37758cad68e4b5e0de2b652e2e2ce699bc9c6d0d
Michel Lavoie <lavoie.michel@gmail.com>
2013-12-09 04:11:22 -0500

    Développé le module cmucam pour utiliser le port série /dev/ttyO0

--- modules/cmucam.py
+++ modules/cmucam.py
@@ -6,23 +6,89 @@
 from time import sleep
 
 # Librairies spéciales
-#======================
-from Adafruit_I2C import Adafruit_I2C
+#=====================
+import serial
 
-# Les pins suivantes peuvent être utiliées (i2c port 1) :
+#===============================================================================
+# Classe :
+# Description :
+#===============================================================================
+class Cmucam:
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyO0')
+        self.ser.baudrate = 115200
+        self.ser.bytesize = 8
+        self.ser.parity = 'N'
+        self.ser.stopbits = 1
+        self.ser.timeout = 1
+        self.ser.xonxoff = 0
+        self.ser.rtscts = 0
 
-# P9_19: I2C2, SCL
-# P9_20: I2C2, SDA
+        # Raw Mode (disable ACK\r et NCK\r)
+        self.ser.write('rm 2\r')
 
-def connect():
-    i2c = Adafruit_I2C(0x77)
+        # Packets Skipped (à confirmer)
+        # Pour ralentir le débit de données
+        self.ser.write('ps 1\r')
+
+        # Set Tracked
+        # (Rmin Rmax Gmin Gmax Bmin Bmax)
+        self.tc = None
+        self.set_tracked()
+
+        # Poll Mode
+        self.poll_mode(False)
+
+    def save_tc(self):
+        with open('tc.txt', 'w') as f:
+            f.write(self.tc)
+
+    def load_tc(self):
+        try:
+            with open('tc.txt', 'r') as f:
+                self.tc = f.readline(eol='\r')
+        except IOError:
+            self.tc = self.get_mean()
+            self.save_tc()
+
+    def get_mean(self):
+        self.ser.write('gm\r')
+        self.tc = self.ser.readline(eol='\r')[2:]
+
+    def poll_mode(self, state):
+        if state:
+            self.pm = True
+            self.ser.write('pm 1\r')
+        else:
+            self.pm = False
+            self.ser.write('pm 0\r')
+
+    def set_tracked(self):
+        if self.tc is None:
+            self.load_tc()
+        self.ser.write('st {0}\r'.format(self.tc))
+
+    def track(self):
+        self.ser.write('tc\r')
+
+#===============================================================================
+# Fonction :
+# Description :
+#===============================================================================
+def track(cam, conn, delay=0.01):
+    cam.poll_mode(False)
+    cam.track()
+    while True:
+        print cam.ser.readline()
+        sleep(delay)
 
 #===============================================================================
 # Fonction :
 # Description :
 #===============================================================================
 def main():
-    pass
+    cam = Cmucam()
+    track(cam)
 
 if __name__ == '__main__':
     main()
