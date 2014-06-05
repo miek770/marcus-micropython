4b05e4bb7ba8118b57a4d5091c13d8d82267a77d
Michel Lavoie <lavoie.michel@gmail.com>
2014-06-04 21:43:09 -0400

    Ajoute le module GP2D12 qui permet de controle (surprise) des GP2D12 de Sharp. J'ai cree une fonction get_dist() qui rettourne une valeur en centimetres pour la pin en question (pin ou canal).

---
+++ modules/gp2d12.py
@@ -0,0 +1,34 @@
+#!/root/marcus/bin/python
+#-*- coding: utf-8 -*-
+
+# Librairies standard
+#=====================
+from time import sleep
+
+# Librairies spéciales
+#======================
+import Adafruit_BBIO.ADC as ADC
+from pins import get_adc
+
+ADC.setup()
+
+#===============================================================================
+# Fonction :    get_dist
+# Description : Retourne la distance en centimetres. La formule a ete calculee
+#               a partir de la fiche technique du manufacturier et de calculs
+#               manuels pour trouver une courbe qui se rapproche des donnees.
+#
+#               L'ecart moyen est de -0,3% dans mes calculs, soit certainement
+#               inferieur a la precision du GP2D12.
+#
+#               L'equation calculee est : y = 8,2/x + 0,04
+#               Ou x = distance en centimetres
+#                  y = lecture analogique (de 0,00 a 1,00)
+#===============================================================================
+def get_dist(pin):
+    d = abs(8.20/(get_adc(pin)-0.04))
+    if d > 80:
+        d = 100
+    elif d < 10:
+        d = 0
+    return d
