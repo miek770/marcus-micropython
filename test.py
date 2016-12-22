1e22c13ccbbb6ef93de340bad2d1808333abe230
Michel Lavoie <lavoie.michel@gmail.com>
2016-12-22 09:47:46 -0500

    Ajustements tests

--- test.py
+++ test.py
@@ -46,26 +46,26 @@ class TestPins(unittest.TestCase):
 
     def test_imports(self):
         try:
-            import modules.pins
+            import peripheriques.pins
 
         except ImportError:
             self.fail()
 
     def test_set_low(self):
         import Adafruit_BBIO.GPIO as GPIO
-        from modules.pins import set_low, set_output, get_input
+        from peripheriques.pins import set_low, set_output, get_input
         pin = "P8_11"
-        set_output(pin, None)
-        set_low(pin, None)
-        self.assertEqual(get_input(pin, None), GPIO.LOW)
+        set_output(pin)
+        set_low(pin)
+        self.assertEqual(get_input(pin), GPIO.LOW)
 
     def test_set_high(self):
         import Adafruit_BBIO.GPIO as GPIO
-        from modules.pins import set_high, set_output, get_input
+        from peripheriques.pins import set_high, set_output, get_input
         pin = "P8_11"
-        set_output(pin, None)
-        set_high(pin, None)
-        self.assertEqual(get_input(pin, None), GPIO.HIGH)
+        set_output(pin)
+        set_high(pin)
+        self.assertEqual(get_input(pin), GPIO.HIGH)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
