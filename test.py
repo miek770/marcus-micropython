44bed596f2673b89d3a051128db508c05b58b641
Michel Lavoie <lavoie.michel@gmail.com>
2016-12-23 20:21:13 -0500

    Trucs mineurs

--- test.py
+++ test.py
@@ -2,31 +2,41 @@
 
 # Librairies standards
 #======================
-import unittest
+import unittest, sys, os
 
 class TestMain(unittest.TestCase):
 
     def test_sys_path(self):
-        import sys
         self.assertIn("/root/marcus", sys.path)
 
     def test_working_directory(self):
-        import os
         self.assertEqual("/root/marcus", os.getcwd())
 
     def test_imports(self):
         try:
             from main import Marcus
-
         except ImportError:
             self.fail()
 
 class TestPins(unittest.TestCase):
 
-    def test_imports(self):
+    def test_import_adafruit(self):
+        try:
+            import Adafruit_BBIO
+            del Adafruit_BBIO
+        except ImportError:
+            self.fail()
+
+    def test_import_serial(self):
+        try:
+            import serial
+        except ImportError:
+            self.fail()
+
+    def test_import_pins(self):
         try:
             import peripheriques.pins
-
+            del peripheriques.pins
         except ImportError:
             self.fail()
 
@@ -45,6 +55,14 @@ class TestPins(unittest.TestCase):
         set_output(pin)
         set_high(pin)
         self.assertEqual(get_input(pin), GPIO.HIGH)
+
+    def test_pwm(self):
+        from peripheriques.pins import set_pwm, set_duty_cycle, reset_pwm
+        pin = "P9_16"
+        set_pwm(pin)
+        set_duty_cycle(pin, 50)
+        reset_pwm(pin)
+        pass
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
